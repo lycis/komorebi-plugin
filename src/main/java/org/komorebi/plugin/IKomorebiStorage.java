7ff9911d585cb22c14b68ed3dedbc4a1bc61f893
lycis <daniel@deder.at>
2014-01-28 13:03:38 +0100

    initial commit

---
+++ src/main/java/org/komorebi/plugin/IKomorebiStorage.java
@@ -0,0 +1,12 @@
+package org.komorebi.plugin;
+
+/**
+ * This interface defines a storage location for Komorebi. This is any place where it may place chunks
+ * of the files that are stored in the system.
+ * 
+ * @author lycis
+ *
+ */
+public interface IKomorebiStorage {
+
+}
