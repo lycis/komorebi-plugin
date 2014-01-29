4a58d7bb7f064dc419ac4cdeace3476ac48f4a39
lycis <daniel@deder.at>
2014-01-29 09:20:10 +0100

    base class IKomorebiPlugin: configuration consumer is defined by returning true on IKomorebiPlugin.isConfigurationConsumer() and requires PlugIn to implement IKomorebiConfigurationConsumer

--- src/main/java/org/komorebi/plugin/IKomorebiPlugin.java
+++ src/main/java/org/komorebi/plugin/IKomorebiPlugin.java
@@ -1,5 +1,18 @@
 package org.komorebi.plugin;
 
+/**
+ * Base interface for all PlugIn types.
+ * 
+ * @author lycis
+ *
+ */
 public interface IKomorebiPlugin {
-
+	/**
+	 * Identifies this PlugIn as configuration consumer and causes the core to publish
+	 * configuration values by access through the interface IKomorebiConfigurationConsumer.
+	 * 
+	 * A PlugIn that returns <code>true</code> on this method has to implement
+	 * IKomorebiConfigurationConsumer.
+	 */
+	boolean isConfigConsumer();
 }
